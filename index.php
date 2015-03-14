e7a1706f52999e9175e4aaf60e1b89c06c385b3f
Francesco <francesco.brundu@gmail.com>
2015-03-14 14:18:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+        	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                <meta name="GENERATOR" content="Mozilla/4.77 [en] (Win95; U) [Netscape]"/>
+                <link rel="stylesheet" type="text/css" href="../style.css" />
+                <title>phiGuest</title>
+        </head>
+        <body>
+	 	<h3>Guestbook</h3>
+		<? include("guestb.php"); ?>
+	</body>
+</html>
+		      
